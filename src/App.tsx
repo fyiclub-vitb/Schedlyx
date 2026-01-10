72ce28f9227bd63da9281b07f5c9ca3e410ff6a6
Parth Gupta <guptaparth2209@gmail.com>
2026-01-10 21:50:21 +0530

    routes added for event cards

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { CreateEvent } from './pages/CreateEvent'
 import { PublicEventPage } from './pages/PublicEventPage'
 import { BookingPage } from './pages/BookingPage'
 import { AdminEventManager } from './pages/AdminEventManager'
+import { EventsList } from './pages/EventsList'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/events" element={<EventsList />} />
         <Route path="/create-event" element={<CreateEvent />} />
         <Route path="/event/:eventId" element={<PublicEventPage />} />
         <Route path="/book/:eventId" element={<BookingPage />} />
